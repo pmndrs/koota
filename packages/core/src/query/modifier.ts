80c5038c3969ce16c53c1c98c07bccc61677efc1
Kris Baumgartner <kjbaumgartner@gmail.com>
2024-08-18 11:09:57 -0400

    ⚡️ (core): Query hashing is even faster

--- packages/core/src/query/modifier.ts
+++ packages/core/src/query/modifier.ts
@@ -1,15 +1,23 @@
+import { $componentId } from '../component/symbols';
 import { Component } from '../component/types';
-import { $modifier, $modifierID } from './symbols';
+import { $modifier, $modifierComponentIds, $modifierID } from './symbols';
 import { Modifier } from './types';
 
 type Fn = (...components: Component[]) => Component[];
 
 export function modifier<T extends Fn>(name: string, id: number, fn: T): Modifier {
 	return function _modifier(...components: Component[]) {
+		const componentIds = components.map((component) => component[$componentId]);
+
 		const returnFn = function () {
 			return fn(...components);
 		};
-		Object.assign(returnFn, { [$modifier]: name, [$modifierID]: id });
+
+		Object.assign(returnFn, {
+			[$modifier]: name,
+			[$modifierID]: id,
+			[$modifierComponentIds]: componentIds,
+		});
 
 		return returnFn;
 	} as Modifier;
