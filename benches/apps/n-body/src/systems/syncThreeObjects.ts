9e4e85c7b387f0a045abed58d8d75868f6cbb215
Kris Baumgartner <kjbaumgartner@gmail.com>
2024-10-10 15:26:45 -0400

    ♻️ n-body: update for latest API

--- benches/apps/n-body/src/systems/syncThreeObjects.ts
+++ benches/apps/n-body/src/systems/syncThreeObjects.ts
@@ -1,7 +1,6 @@
 import { Circle, Color, Position } from '@sim/n-body';
 import * as THREE from 'three';
 import { InstancedMesh } from '../components/InstancedMesh';
-import { getIndex } from 'koota';
 
 const normalize = (x: number, min: number, max: number) => (x - min) / (max - min);
 
@@ -15,22 +14,20 @@ export const syncThreeObjects = ({ world }: { world: Koota.World }) => {
 	const instancedMesh = instanceEnt.get(InstancedMesh)!.object;
 
 	world.query(Position, Circle, Color).updateEach(([position, circle, color], entity) => {
-		const i = getIndex(entity);
-
 		dummy.position.set(position.x, position.y, 0);
 
 		const radius = normalize(circle.radius, 0, 60);
 		dummy.scale.set(radius, radius, radius);
 
 		dummy.updateMatrix();
-		instancedMesh.setMatrixAt(i, dummy.matrix);
+		instancedMesh.setMatrixAt(entity.id(), dummy.matrix);
 
 		dummyColor.setRGB(
 			normalize(color.r, 0, 255),
 			normalize(color.g, 0, 255),
 			normalize(color.b, 0, 255)
 		);
-		instancedMesh.setColorAt(i, dummyColor);
+		instancedMesh.setColorAt(entity.id(), dummyColor);
 	});
 
 	instancedMesh.instanceMatrix.needsUpdate = true;
--- benches/sims/n-body/src/systems/updateGravity.ts
+++ benches/sims/n-body/src/systems/updateGravity.ts
@@ -1,38 +1,39 @@
-import { getIndex } from 'koota';
 import { Time } from '../components/Time';
 import { CONSTANTS } from '../constants';
 import { bodyTraits } from './setInitial';
 
 export const updateGravity = ({ world }: { world: Koota.World }) => {
-	const ents = world.query(...bodyTraits);
+	const bodies = world.query(...bodyTraits);
 	const { delta } = world.get(Time)!;
-	const [position, velocity, mass, _, acceleration] = world.getStore(...bodyTraits);
 
-	for (let j = 0; j < ents.length; j++) {
-		const eSelf = getIndex(ents[j]);
+	bodies.useStores(([position, velocity, mass, _, acceleration], bodies) => {
+		for (let j = 0; j < bodies.length; j++) {
+			const currentId = bodies[j].id();
 
-		acceleration.x[eSelf] = 0;
-		acceleration.y[eSelf] = 0;
+			acceleration.x[currentId] = 0;
+			acceleration.y[currentId] = 0;
 
-		for (let i = 0; i < ents.length; i++) {
-			const eTarget = getIndex(ents[i]);
-			if (eSelf === eTarget) continue; // Skip self
+			for (let i = 0; i < bodies.length; i++) {
+				const targetId = bodies[i].id();
+				if (currentId === targetId) continue; // Skip self
 
-			const dx = +position.x[eTarget] - +position.x[eSelf];
-			const dy = +position.y[eTarget] - +position.y[eSelf];
-			let distanceSquared = dx * dx + dy * dy;
+				const dx = +position.x[targetId] - +position.x[currentId];
+				const dy = +position.y[targetId] - +position.y[currentId];
+				let distanceSquared = dx * dx + dy * dy;
 
-			if (distanceSquared < CONSTANTS.STICKY) distanceSquared = CONSTANTS.STICKY; // Apply stickiness
+				if (distanceSquared < CONSTANTS.STICKY) distanceSquared = CONSTANTS.STICKY; // Apply stickiness
 
-			const distance = Math.sqrt(distanceSquared);
-			const forceMagnitude = (+mass.value[eSelf] * +mass.value[eTarget]) / distanceSquared;
+				const distance = Math.sqrt(distanceSquared);
+				const forceMagnitude =
+					(+mass.value[currentId] * +mass.value[targetId]) / distanceSquared;
 
-			acceleration.x[eSelf] += (dx / distance) * forceMagnitude;
-			acceleration.y[eSelf] += (dy / distance) * forceMagnitude;
+				acceleration.x[currentId] += (dx / distance) * forceMagnitude;
+				acceleration.y[currentId] += (dy / distance) * forceMagnitude;
+			}
+
+			// Apply computed force to entity's velocity, adjusting for its mass
+			velocity.x[currentId] += (acceleration.x[currentId] * delta) / mass.value[currentId];
+			velocity.y[currentId] += (acceleration.y[currentId] * delta) / mass.value[currentId];
 		}
-
-		// Apply computed force to entity's velocity, adjusting for its mass
-		velocity.x[eSelf] += (acceleration.x[eSelf] * delta) / mass.value[eSelf];
-		velocity.y[eSelf] += (acceleration.y[eSelf] * delta) / mass.value[eSelf];
-	}
+	});
 };
