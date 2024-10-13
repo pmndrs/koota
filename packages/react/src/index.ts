b30c9fb257ef25f58cb055a7a0c4982f1df4c135
Kris Baumgartner <kjbaumgartner@gmail.com>
2024-10-13 16:00:03 -0400

    🐛 (react): Export createActions and useObserve

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -1,3 +1,5 @@
 export { useQuery } from './query/use-query';
 export { WorldProvider } from './world/world-provider';
 export { useWorld } from './world/use-world';
+export { createActions } from './actions/create-actions';
+export { useObserve } from './trait/use-observe';
