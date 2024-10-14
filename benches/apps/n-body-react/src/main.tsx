6d3e0852544808b9105e597439153fb88db04993
Kris Baumgartner <kjbaumgartner@gmail.com>
2024-10-13 23:46:33 -0400

    ♻️ (n-body-react): Update for new API

--- benches/apps/n-body-react/src/main.tsx
+++ benches/apps/n-body-react/src/main.tsx
@@ -2,9 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App.tsx';
 import './index.css';
+import { WorldProvider } from 'koota/react';
+import { world } from '@sim/n-body';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
-		<App />
+		<WorldProvider world={world}>
+			<App />
+		</WorldProvider>
 	</React.StrictMode>
 );
--- benches/apps/n-body-react/src/systems/cleanupRepulsors.ts
+++ benches/apps/n-body-react/src/systems/cleanupRepulsors.ts
@@ -1,22 +1,20 @@
-import { Repulse, Position } from '@sim/n-body/src/components';
-import { createRemoved } from 'koota';
-import { InstancedMesh } from 'koota/react';
+import { Position, Repulse } from '@sim/n-body';
+import { createRemoved, World } from 'koota';
 import * as THREE from 'three';
+import { InstancedMesh } from '../traits/InstancedMesh';
 
 const Removed = createRemoved();
 const zeroScaleMatrix = new THREE.Matrix4().makeScale(0, 0, 0);
 
-export function cleanupBodies({ world }: { world: Koota.World }) {
-	const ents = world.query(Removed(Repulse, Position));
+export function cleanupBodies({ world }: { world: World }) {
+	const instanceEntity = world.queryFirst(InstancedMesh);
+	if (instanceEntity === undefined) return;
 
-	const instanceEnt = world.query(InstancedMesh)[0];
-	if (instanceEnt === undefined) return;
+	const instancedMesh = instanceEntity.get(InstancedMesh)!.object;
 
-	const instancedMesh = world.get(InstancedMesh).object[instanceEnt];
-
-	for (const e of ents) {
-		instancedMesh.setMatrixAt(e, zeroScaleMatrix);
-	}
+	world.query(Removed(Repulse, Position)).forEach((e) => {
+		instancedMesh.setMatrixAt(e.id(), zeroScaleMatrix);
+	});
 
 	instancedMesh.instanceMatrix.needsUpdate = true;
 }
