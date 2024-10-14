c2ae821d8d31b575ffcf82e2f9d7641c61caad8a
Kris Baumgartner <kjbaumgartner@gmail.com>
2024-10-14 14:17:56 -0400

    ♻️ add-remove: update for passive option

--- benches/sims/add-remove/src/systems/updateGravity.ts
+++ benches/sims/add-remove/src/systems/updateGravity.ts
@@ -5,8 +5,11 @@ import { Velocity, Time } from '../trait';
 export const updateGravity = ({ world }: { world: World }) => {
 	const { delta } = world.get(Time);
 
-	world.query(Velocity).updateEach(([velocity]) => {
-		// Apply gravity directly to the velocity
-		velocity.y += CONSTANTS.GRAVITY * delta;
-	});
+	world.query(Velocity).updateEach(
+		([velocity]) => {
+			// Apply gravity directly to the velocity
+			velocity.y += CONSTANTS.GRAVITY * delta;
+		},
+		{ passive: true }
+	);
 };
