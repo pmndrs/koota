88576ca252559ebfc455dc65166e59ee97a334b7
Kris Baumgartner <kjbaumgartner@gmail.com>
2024-10-17 13:50:54 -0400

    🐛 (react): Export useEntityRef

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -3,3 +3,4 @@ export { WorldProvider } from './world/world-provider';
 export { useWorld } from './world/use-world';
 export { createActions } from './actions/create-actions';
 export { useObserve } from './hooks/use-observe';
+export { useEntityRef } from './hooks/use-entity-ref';
