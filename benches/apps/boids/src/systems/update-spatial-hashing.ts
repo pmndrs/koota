8c0692ca3ad7d72a7e7b09b19e5af3467650e5d4
Kris Baumgartner <kjbaumgartner@gmail.com>
2024-10-18 15:53:25 -0400

    ♻️ (benches): Update for new API

--- benches/apps/boids/src/systems/update-spatial-hashing.ts
+++ benches/apps/boids/src/systems/update-spatial-hashing.ts
@@ -4,10 +4,7 @@ import { Position, SpatialHashMap } from '../traits';
 export const updateSpatialHashing = ({ world }: { world: World }) => {
 	const { value: spatialHashMap } = world.get(SpatialHashMap);
 
-	world.query(Position).updateEach(
-		([{ value: position }], entity) => {
-			spatialHashMap.setEntity(entity, position.x, position.y, position.z);
-		},
-		{ passive: true }
-	);
+	world.query(Position).updateEach(([{ value: position }], entity) => {
+		spatialHashMap.setEntity(entity, position.x, position.y, position.z);
+	});
 };
