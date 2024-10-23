fac5d3213d904ecdcab50d6520e5449432d174d0
Kris Baumgartner <kjbaumgartner@gmail.com>
2024-10-23 14:49:55 -0400

    🏷️ react: fix types

--- packages/react/src/hooks/use-observe.ts
+++ packages/react/src/hooks/use-observe.ts
@@ -1,10 +1,10 @@
-import { Entity, ExtractSchema, Trait, TraitInstanceFromSchema } from '@koota/core';
+import { Entity, Trait, TraitInstance } from '@koota/core';
 import { useEffect, useState } from 'react';
 import { useWorld } from '../world/use-world';
 
 export function useObserve<T extends Trait>(entity: Entity, trait: T) {
 	const world = useWorld();
-	const [value, setValue] = useState<TraitInstanceFromSchema<ExtractSchema<T>> | undefined>(() => {
+	const [value, setValue] = useState<TraitInstance<T> | undefined>(() => {
 		if (entity.has(trait)) return entity.get(trait);
 		return undefined;
 	});
