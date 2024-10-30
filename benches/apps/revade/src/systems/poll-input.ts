a32035c12ce9ca62d0bb2f949b74c1507b80eaca
Kris Baumgartner <kjbaumgartner@gmail.com>
2024-10-29 21:56:07 -0400

    ✨ revade: add wasd

--- benches/apps/revade/src/systems/poll-input.ts
+++ benches/apps/revade/src/systems/poll-input.ts
@@ -8,21 +8,33 @@ const keys = {
 	arrowDown: false,
 	arrowLeft: false,
 	arrowRight: false,
+	w: false,
+	a: false,
+	s: false,
+	d: false,
 };
 
 window.addEventListener('keydown', (e) => {
 	switch (e.key.toLowerCase()) {
 		case 'arrowup':
+		case 'w':
 			keys.arrowUp = true;
+			keys.w = true;
 			break;
 		case 'arrowdown':
+		case 's':
 			keys.arrowDown = true;
+			keys.s = true;
 			break;
 		case 'arrowleft':
+		case 'a':
 			keys.arrowLeft = true;
+			keys.a = true;
 			break;
 		case 'arrowright':
+		case 'd':
 			keys.arrowRight = true;
+			keys.d = true;
 			break;
 	}
 });
@@ -30,16 +42,24 @@ window.addEventListener('keydown', (e) => {
 window.addEventListener('keyup', (e) => {
 	switch (e.key.toLowerCase()) {
 		case 'arrowup':
+		case 'w':
 			keys.arrowUp = false;
+			keys.w = false;
 			break;
 		case 'arrowdown':
+		case 's':
 			keys.arrowDown = false;
+			keys.s = false;
 			break;
 		case 'arrowleft':
+		case 'a':
 			keys.arrowLeft = false;
+			keys.a = false;
 			break;
 		case 'arrowright':
+		case 'd':
 			keys.arrowRight = false;
+			keys.d = false;
 			break;
 	}
 });
@@ -47,8 +67,8 @@ window.addEventListener('keyup', (e) => {
 export const pollInput = ({ world }: { world: World }) => {
 	world.query(IsPlayer, Input).updateEach(([direction]) => {
 		// Get horizontal and vertical input.
-		const horizontal = (keys.arrowRight ? 1 : 0) - (keys.arrowLeft ? 1 : 0);
-		const vertical = (keys.arrowUp ? 1 : 0) - (keys.arrowDown ? 1 : 0);
+		const horizontal = (keys.arrowRight || keys.d ? 1 : 0) - (keys.arrowLeft || keys.a ? 1 : 0);
+		const vertical = (keys.arrowUp || keys.w ? 1 : 0) - (keys.arrowDown || keys.s ? 1 : 0);
 
 		// Normalize the vector if moving diagonally.
 		const length = Math.sqrt(horizontal * horizontal + vertical * vertical);
