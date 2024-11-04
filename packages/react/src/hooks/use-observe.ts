803c735ed32b2dcaa10279e6c62f0e50b8f39069
Kris Baumgartner <kjbaumgartner@gmail.com>
2024-11-04 18:02:28 -0500

    🐛 (react): Track adds and removes in useObserve

--- packages/react/src/hooks/use-observe.ts
+++ packages/react/src/hooks/use-observe.ts
@@ -3,10 +3,13 @@ import { useEffect, useMemo, useState } from 'react';
 import { useWorld } from '../world/use-world';
 
 function isWorld(target: Entity | World): target is World {
-	return typeof (target as World).spawn === 'function';
+	return typeof (target as World)?.spawn === 'function';
 }
 
-export function useObserve<T extends Trait>(target: Entity | World, trait: T) {
+export function useObserve<T extends Trait>(
+	target: Entity | World,
+	trait: T
+): TraitInstance<T> | undefined {
 	const contextWorld = useWorld();
 	const world = useMemo(() => (isWorld(target) ? target : contextWorld), [target, contextWorld]);
 	const entity = useMemo(
@@ -20,12 +23,22 @@ export function useObserve<T extends Trait>(target: Entity | World, trait: T) {
 	});
 
 	useEffect(() => {
-		const unsub = world.onChange(trait, (e) => {
+		const onChangeUnsub = world.onChange(trait, (e) => {
 			if (e === entity) setValue(e.get(trait));
 		});
 
+		const onAddUnsub = world.onAdd([trait], (e) => {
+			if (e === entity) setValue(e.get(trait));
+		});
+
+		const onRemoveUnsub = world.onRemove([trait], (e) => {
+			if (e === entity) setValue(undefined);
+		});
+
 		return () => {
-			unsub();
+			onChangeUnsub();
+			onAddUnsub();
+			onRemoveUnsub();
 		};
 	}, [target, trait]);
 
