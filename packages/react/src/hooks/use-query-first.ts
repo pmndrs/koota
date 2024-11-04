3844f22a31e42c88f9ad4477ff579d342113bc50
Kris Baumgartner <kjbaumgartner@gmail.com>
2024-11-04 18:03:02 -0500

    ✨ (react): Add useQueryFirst hook

---
+++ packages/react/src/hooks/use-query-first.ts
@@ -0,0 +1,7 @@
+import { Entity, QueryParameter } from '@koota/core';
+import { useQuery } from './use-query';
+
+export function useQueryFirst<T extends QueryParameter[]>(...parameters: T): Entity | undefined {
+	const query = useQuery(...parameters);
+	return query[0];
+}
