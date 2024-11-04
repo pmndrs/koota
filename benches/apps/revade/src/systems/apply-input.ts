02b2547d65984292ab853afae659d2acc90f762f
Kris Baumgartner <kjbaumgartner@gmail.com>
2024-11-04 18:09:31 -0500

    🐛 (revade): Make player acceleration framerate independent

--- benches/apps/revade/src/systems/apply-input.ts
+++ benches/apps/revade/src/systems/apply-input.ts
@@ -1,15 +1,16 @@
 import { World } from 'koota';
-import { IsPlayer, Transform, Movement, Input } from '../traits';
+import { IsPlayer, Transform, Movement, Input, Time } from '../traits';
 import * as THREE from 'three';
 
 const UP = new THREE.Vector3(0, 1, 0);
 const tmpvec3 = new THREE.Vector3();
 
 export const applyInput = ({ world }: { world: World }) => {
+	const { delta } = world.get(Time);
 	world
 		.query(IsPlayer, Input, Transform, Movement)
 		.updateEach(([input, transform, { velocity, thrust }]) => {
-			velocity.add(tmpvec3.set(input.x, input.y, 0).multiplyScalar(thrust));
+			velocity.add(tmpvec3.set(input.x, input.y, 0).multiplyScalar(thrust * delta * 100));
 			transform.quaternion.setFromUnitVectors(UP, velocity.clone().normalize());
 		});
 };
