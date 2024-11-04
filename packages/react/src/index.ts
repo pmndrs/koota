7eaacbc1f4599d4374664b78a30e6ebf49aae826
Kris Baumgartner <kjbaumgartner@gmail.com>
2024-11-04 18:03:02 -0500

    ✨ react: add useQueryFirst hook

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -4,3 +4,4 @@ export { useWorld } from './world/use-world';
 export { createActions } from './actions/create-actions';
 export { useObserve } from './hooks/use-observe';
 export { useEntityRef } from './hooks/use-entity-ref';
+export { useQueryFirst } from './hooks/use-query-first';
