3f72e62931d361f0491fa75323105a8901ba9670
Kris Baumgartner <kjbaumgartner@gmail.com>
2024-11-07 10:16:17 -0500

    ✨ react: add useObservePassive -- likely to be renamed

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -5,3 +5,4 @@ export { createActions } from './actions/create-actions';
 export { useObserve } from './hooks/use-observe';
 export { useEntityRef } from './hooks/use-entity-ref';
 export { useQueryFirst } from './hooks/use-query-first';
+export { useObservePassive } from './hooks/use-observe-passive';
