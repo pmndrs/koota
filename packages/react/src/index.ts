2fd014666f694232f9fe4e94ddf9aa0515642c5c
Kris Baumgartner <kjbaumgartner@gmail.com>
2024-11-11 21:25:11 -0500

    🔥 react: remove useEntityRef

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -3,6 +3,5 @@ export { WorldProvider } from './world/world-provider';
 export { useWorld } from './world/use-world';
 export { createActions } from './actions/create-actions';
 export { useObserve } from './hooks/use-observe';
-export { useEntityRef } from './hooks/use-entity-ref';
 export { useQueryFirst } from './hooks/use-query-first';
 export { useObservePassive } from './hooks/use-observe-passive';
