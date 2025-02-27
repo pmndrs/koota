5608b08d7c8957473fae6eeb1a7c6d8328d6dd0e
Kris Baumgartnerm <kjbaumgartner@gmail.com>
2025-02-27 16:42:03 -0500

    💥 react: useQuery is correclty immutable

--- packages/react/src/hooks/use-query.ts
+++ packages/react/src/hooks/use-query.ts
@@ -1,42 +1,41 @@
-import { Entity, QueryParameter, QueryResult } from '@koota/core';
-import { useEffect, useMemo, useReducer } from 'react';
+import { $internal, cacheQuery, QueryParameter, QueryResult } from '@koota/core';
+import { useEffect, useMemo, useState } from 'react';
 import { useWorld } from '../world/use-world';
 
 export function useQuery<T extends QueryParameter[]>(...parameters: T): QueryResult<T> {
 	const world = useWorld();
-	const entities = useMemo(() => world.query(...parameters), [world, ...parameters]);
-	const [, forceUpdate] = useReducer((v) => v + 1, 0);
 
-	// Set entities at effect time
-	useEffect(() => {
-		const mutableEntities = entities as unknown as Entity[];
-		mutableEntities.length = 0;
-		mutableEntities.push(...world.query(...parameters));
+	const [hash, initialVersion] = useMemo(() => {
+		const hash = cacheQuery(...parameters);
+		// Using internals to get the query data
+		const query = world[$internal].queriesHashMap.get(hash)!;
+		return [hash, query.version];
+	}, [parameters]);
 
-		forceUpdate();
-	}, [world]);
+	const [entities, setEntities] = useState<QueryResult<T>>(() => world.query(hash));
 
 	// Subscribe to changes
 	useEffect(() => {
-		const unsubAdd = world.onAdd(parameters, (entity) => {
-			const mutableEntities = entities as unknown as Entity[];
-			mutableEntities.push(entity);
-			forceUpdate();
+		const unsubAdd = world.onAdd(parameters, () => {
+			setEntities(world.query(hash));
 		});
 
-		const unsubRemove = world.onRemove(parameters, (entity) => {
-			const mutableEntities = entities as unknown as Entity[];
-			const index = mutableEntities.indexOf(entity);
-			mutableEntities[index] = mutableEntities[mutableEntities.length - 1];
-			mutableEntities.pop();
-			forceUpdate();
+		const unsubRemove = world.onRemove(parameters, () => {
+			setEntities(world.query(hash));
 		});
+
+		// Compare the initial version to the current version to
+		// see it the query has changed
+		const query = world[$internal].queriesHashMap.get(hash)!;
+		if (query.version !== initialVersion) {
+			setEntities(world.query(hash));
+		}
 
 		return () => {
 			unsubAdd();
 			unsubRemove();
 		};
-	}, [world]);
+	}, [world, hash]);
 
 	return entities;
 }
