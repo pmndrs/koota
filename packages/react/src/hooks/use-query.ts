feb86f892c4631d9c8edc54ec7e047e1aaf5a126
Kris Baumgartnerm <kjbaumgartner@gmail.com>
2025-03-22 16:59:37 -0400

    🐛 react: fix reset reactivity in useQuery

--- packages/react/src/hooks/use-query.ts
+++ packages/react/src/hooks/use-query.ts
@@ -4,13 +4,14 @@ import { useWorld } from '../world/use-world';
 
 export function useQuery<T extends QueryParameter[]>(...parameters: T): QueryResult<T> {
 	const world = useWorld();
+	const [version, setVersion] = useState(0);
 
-	const [hash, initialVersion] = useMemo(() => {
+	const [hash, initialQueryVersion] = useMemo(() => {
 		const hash = cacheQuery(...parameters);
 		// Using internals to get the query data
 		const query = world[$internal].queriesHashMap.get(hash)!;
 		return [hash, query.version];
-	}, [parameters]);
+	}, [parameters, world]);
 
 	const [entities, setEntities] = useState<QueryResult<T>>(() => world.query(hash).sort());
 
@@ -27,7 +28,7 @@ export function useQuery<T extends QueryParameter[]>(...parameters: T): QueryRes
 		// Compare the initial version to the current version to
 		// see it the query has changed
 		const query = world[$internal].queriesHashMap.get(hash)!;
-		if (query.version !== initialVersion) {
+		if (query.version !== initialQueryVersion) {
 			setEntities(world.query(hash).sort());
 		}
 
@@ -35,7 +36,17 @@ export function useQuery<T extends QueryParameter[]>(...parameters: T): QueryRes
 			unsubAdd();
 			unsubRemove();
 		};
-	}, [world, hash]);
+	}, [world, hash, version]);
+
+	// Force reattaching event listeners when the world is reset
+	useEffect(() => {
+		const handler = () => setVersion((v) => v + 1);
+		world[$internal].resetSubscriptions.add(handler);
+
+		return () => {
+			world[$internal].resetSubscriptions.delete(handler);
+		};
+	}, [world]);
 
 	return entities;
 }
