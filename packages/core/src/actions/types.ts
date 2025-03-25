ee666029f1bc0f41c4380417b244cf09b7ce162d
Kris Baumgartnerm <kjbaumgartner@gmail.com>
2025-03-24 21:16:03 -0400

    🏷️ core: improve action types

---
+++ packages/core/src/actions/types.ts
@@ -0,0 +1,5 @@
+import { World } from '../world/world';
+
+export type Actions = Record<string, (...args: any[]) => void>;
+export type ActionInitializer<T extends Actions> = (world: World) => T;
+export type ActionGetter<T extends Actions> = (world: World) => T;
