ad6cf7c73fcf6efa9ac3b18ff90de753680d657f
Justin Walsh <contact.me@thejustinwalsh.com>
2025-05-02 01:54:58 -0400

    fix: emit use strict everywhere to thwart bad bundlers from running non-strict

--- packages/publish/tsup.config.ts
+++ packages/publish/tsup.config.ts
@@ -3,6 +3,11 @@ import { defineConfig } from 'tsup';
 export default defineConfig({
 	entry: ['src/index.ts', 'src/react.ts'],
 	format: ['esm', 'cjs'],
+	esbuildOptions: (options, { format }) => {
+		options.banner = format === 'esm' ? {
+			js: '\"use strict\";',
+		} : undefined;
+	},
 	dts: {
 		resolve: true,
 	},
