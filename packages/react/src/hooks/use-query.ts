ba631618f7be65e09bdefa151f4a546bc3de7cb9
Kris Baumgartner <kjbaumgartner@gmail.com>
2025-06-11 21:40:13 -0400

    🐛 react: make useQuery rerenders more stable

--- packages/react/src/hooks/use-query.ts
+++ packages/react/src/hooks/use-query.ts
@@ -1,34 +1,39 @@
 import { $internal, cacheQuery, type QueryParameter, type QueryResult } from '@koota/core';
-import { useEffect, useMemo, useState } from 'react';
+import { useEffect, useMemo, useRef, useState } from 'react';
 import { useWorld } from '../world/use-world';
 
 export function useQuery<T extends QueryParameter[]>(...parameters: T): QueryResult<T> {
 	const world = useWorld();
+	const initialQueryVersionRef = useRef(0);
+	// Used to track if we need to rerun effects internally.
 	const [version, setVersion] = useState(0);
 
-	const [hash, initialQueryVersion] = useMemo(() => {
-		const hash = cacheQuery(...parameters);
-		// Using internals to get the query data
+	// This will rerun every render since parameters will always be a fresh
+	// array, but the return value will be stable.
+	const hash = useMemo(() => cacheQuery(...parameters), [parameters]);
+
+	useMemo(() => {
+		// Using internals to get the query data.
 		const query = world[$internal].queriesHashMap.get(hash)!;
-		return [hash, query.version];
-	}, [parameters, world]);
+		initialQueryVersionRef.current = query.version;
+	}, [world, hash]);
 
 	const [entities, setEntities] = useState<QueryResult<T>>(() => world.query(hash).sort());
 
-	// Subscribe to changes
+	// Subscribe to changes.
 	useEffect(() => {
-		const unsubAdd = world.onQueryAdd(parameters, () => {
+		const unsubAdd = world.onQueryAdd(hash, () => {
 			setEntities(world.query(hash).sort());
 		});
 
-		const unsubRemove = world.onQueryRemove(parameters, () => {
+		const unsubRemove = world.onQueryRemove(hash, () => {
 			setEntities(world.query(hash).sort());
 		});
 
 		// Compare the initial version to the current version to
-		// see it the query has changed
+		// see it the query has changed.
 		const query = world[$internal].queriesHashMap.get(hash)!;
-		if (query.version !== initialQueryVersion) {
+		if (query.version !== initialQueryVersionRef.current) {
 			setEntities(world.query(hash).sort());
 		}
 
@@ -38,7 +43,7 @@ export function useQuery<T extends QueryParameter[]>(...parameters: T): QueryRes
 		};
 	}, [world, hash, version]);
 
-	// Force reattaching event listeners when the world is reset
+	// Force reattaching event listeners when the world is reset.
 	useEffect(() => {
 		const handler = () => setVersion((v) => v + 1);
 		world[$internal].resetSubscriptions.add(handler);
