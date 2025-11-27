60c8457234394ba9267c6cfea7df5a7418b197f0
Kris Baumgartner <kjbaumgartner@gmail.com>
2025-11-27 00:39:38 -0500

    ♻️ (revade): Update style, add relations

--- benches/apps/revade/src/actions.ts
+++ benches/apps/revade/src/actions.ts
@@ -1,13 +1,15 @@
-import { createActions, type TraitValue } from 'koota';
+import { createActions, type Entity, type TraitValue } from 'koota';
 import * as THREE from 'three';
 import {
 	AutoRotate,
 	Avoidance,
 	Bullet,
+	FiredBy,
 	Input,
 	IsEnemy,
 	IsPlayer,
 	Movement,
+	Targeting,
 	Transform,
 } from './traits';
 
@@ -17,22 +19,28 @@ export const actions = createActions((world) => ({
 	spawnPlayer: (transform?: TransformValue) => {
 		return world.spawn(IsPlayer, Movement, Input, Transform(transform));
 	},
-	spawnEnemy: (transform?: TransformValue) => {
-		return world.spawn(
+	spawnEnemy: (options: { transform?: TransformValue; target?: Entity }) => {
+		const enemy = world.spawn(
 			IsEnemy,
 			Movement({ thrust: 0.5, damping: 0.98 }),
-			Transform(transform),
+			Transform(options.transform),
 			AutoRotate,
 			Avoidance
 		);
+		if (options.target) enemy.add(Targeting(options.target));
+
+		return enemy;
 	},
-	spawnBullet: (position: THREE.Vector3, rotation: THREE.Quaternion) => {
+	spawnBullet: (position: THREE.Vector3, rotation: THREE.Quaternion, firedBy?: Entity) => {
 		const direction = new THREE.Vector3(0, 1, 0);
 		direction.applyQuaternion(rotation);
 
-		return world.spawn(
+		const bullet = world.spawn(
 			Transform({ position: position.clone(), quaternion: rotation.clone() }),
 			Bullet({ direction })
 		);
+		if (firedBy) bullet.add(FiredBy(firedBy));
+
+		return bullet;
 	},
 }));
