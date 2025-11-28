52a79cb967ebcb3d06c5fabdaa62b1589e2804ac
Kris Baumgarter <kjbaumgartner@gmail.com>
2025-11-28 12:42:52 -0500

    🎉 graph-traversal: add a sim for testing relation traversal

---
+++ benches/sims/graph-traversal/src/index.ts
@@ -0,0 +1,7 @@
+export { CONFIG } from './config';
+export { init } from './systems/init';
+export { traverse } from './systems/traverse';
+export { schedule } from './systems/schedule';
+export * from './traits';
+export { world } from './world';
+
