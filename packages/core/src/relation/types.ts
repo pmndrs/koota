c6d1919dfc21f65f34b1735b52ef4af1f129e909
Kris Baumgartner <kjbaumgartner@gmail.com>
2025-12-03 15:58:05 -0500

    ⚰️ core: more dead code

--- packages/core/src/relation/types.ts
+++ packages/core/src/relation/types.ts
@@ -4,15 +4,6 @@ import type { Trait } from '../trait/types';
 
 export type RelationTarget = Entity | '*';
 
-/** Internal store structure for relation targets */
-export interface RelationStore {
-	/** For exclusive: targets[entityId] = targetEntityId (0 = no target) */
-	/** For non-exclusive: targets[entityId] = array of targetEntityIds */
-	targets: number[] | number[][];
-	/** For data-bearing relations: data[entityId][targetIdx] = user data */
-	data: unknown[][] | null;
-}
-
 /** A pair represents a relation + target combination */
 export interface RelationPair<T extends Trait = Trait> {
 	[$internal]: {
