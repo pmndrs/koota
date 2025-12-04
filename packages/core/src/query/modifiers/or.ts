3441166217d38f53028ac34f9bb950b8f177aab4
Kris Baumgarter <kjbaumgartner@gmail.com>
2025-12-04 12:58:01 -0500

    🏷️ use explicit type imports (#187)
    🏷️ use explicit type imports

--- packages/core/src/query/modifiers/or.ts
+++ packages/core/src/query/modifiers/or.ts
@@ -1,6 +1,7 @@
 import type { Trait } from '../../trait/types';
+import type { ModifierData } from '../types';
 import { createModifier } from '../modifier';
 
-export const Or = <T extends Trait[] = Trait[]>(...traits: T) => {
+export const Or = <T extends Trait[] = Trait[]>(...traits: T): ModifierData<T, 'or'> => {
 	return createModifier('or', 2, traits);
 };
