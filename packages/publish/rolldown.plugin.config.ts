4375e15c6dc3d3415933c94a3b720a5df5d79218
Kris Baumgartner <kjbaumgartner@gmail.com>
2025-12-05 12:33:52 -0500

    💥 goodbye CJS

--- packages/publish/rolldown.plugin.config.ts
+++ packages/publish/rolldown.plugin.config.ts
@@ -1,13 +1,8 @@
 import { defineConfig } from 'rolldown';
 
-const banner = '"use strict";';
-
 export default defineConfig({
 	input: { 'devtools-plugin': 'src/devtools-plugin.ts' },
-	output: [
-		{ format: 'esm', dir: 'dist', entryFileNames: '[name].js', banner },
-		{ format: 'cjs', dir: 'dist', entryFileNames: '[name].cjs' },
-	],
+	output: { format: 'esm', dir: 'dist', entryFileNames: '[name].js' },
 	external: ['unplugin'],
 	platform: 'node',
 });
