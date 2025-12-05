7bdb795f33f4a7a5c84f6a2794400f6a9274fbfe
Kris Baumgartner <kjbaumgartner@gmail.com>
2025-12-04 19:23:47 -0500

    🏷️ devtools: plugin doesn't require options

--- benches/apps/revade/vite.config.ts
+++ benches/apps/revade/vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), vitePlugin({})],
+	plugins: [react(), vitePlugin()],
 	server: {
 		headers: {
 			'Cross-Origin-Embedder-Policy': 'require-corp',
