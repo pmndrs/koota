6a53d74df6fb4f1565cad9be2941144ecabd3792
Kris Baumgartner <kjbaumgartner@gmail.com>
2026-01-09 11:55:53 -0500

    ✨ revade: now usses devtools

--- examples/apps/revade/vite.config.ts
+++ examples/apps/revade/vite.config.ts
@@ -1,13 +1,14 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import kootaDevtools from 'koota/devtools/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
-	server: {
-		headers: {
-			'Cross-Origin-Embedder-Policy': 'require-corp',
-			'Cross-Origin-Opener-Policy': 'same-origin',
-		},
-	},
+    plugins: [react(), kootaDevtools()],
+    server: {
+        headers: {
+            'Cross-Origin-Embedder-Policy': 'require-corp',
+            'Cross-Origin-Opener-Policy': 'same-origin',
+        },
+    },
 });
