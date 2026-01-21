91609a1831053d2beb4b29bf1adcbe295a81acb7
Kris Baumgartner <kjbaumgartner@gmail.com>
2026-01-20 20:12:36 -0500

    💩 restore deleted files

---
+++ examples/apps/cards/src/systems/update-time.ts
@@ -0,0 +1,9 @@
+import type { World } from 'koota';
+import { Time } from '../traits';
+
+export function updateTime(world: World) {
+    const now = performance.now();
+    const time = world.get(Time)!;
+    const delta = Math.min((now - time.last) / 1000, 0.1);
+    world.set(Time, { last: now, delta });
+}
