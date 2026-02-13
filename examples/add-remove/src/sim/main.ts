b779c8612f817a6a2fe178e25c4830f5fd8f4f7c
ospira <24227725+ospira@users.noreply.github.com>
2026-02-13 16:39:12 -0500

    simplify examples

---
+++ examples/add-remove/src/sim/main.ts
@@ -0,0 +1,18 @@
+// Based on Sander Mertens' ECS N-Body simulation for Flecs
+// https://github.com/SanderMertens/ecs_nbody
+
+import { measure, requestAnimationFrame } from '@examples/bench-tools';
+import { init } from './systems/init';
+import { schedule } from './systems/schedule';
+import { world } from './world';
+
+// Start the simulation.
+const main = () => {
+    measure(() => schedule.run({ world }));
+    requestAnimationFrame(main);
+};
+
+// Initialize all entities.
+init({ world });
+
+requestAnimationFrame(main);
--- examples/sims/boids/src/traits/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './position.ts';
-export * from './velocity.ts';
-export * from './time.ts';
-export * from './forces.ts';
-export * from './neighbor-of.ts';
