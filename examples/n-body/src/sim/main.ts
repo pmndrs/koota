fb6b183e4d85707f3b6088479a0970573aad00a6
ospira <24227725+ospira@users.noreply.github.com>
2026-02-22 13:27:21 -0500

    🚚 simplify examples (#231)
    * simplify examples
    
    * update lock file, update intro for new revade location

---
+++ examples/n-body/src/sim/main.ts
@@ -0,0 +1,14 @@
+// Based on Sander Mertens' ECS N-Body simulation for Flecs
+// https://github.com/SanderMertens/ecs_nbody
+
+import { measure, requestAnimationFrame } from '@examples/bench-tools';
+import { schedule } from './systems/schedule';
+import { world } from './world';
+
+// Start the simulation.
+const main = async () => {
+    await measure(() => schedule.run({ world }));
+    requestAnimationFrame(main);
+};
+
+requestAnimationFrame(main);
